314d1b1b2a58713a42b3199ca67c4ec3459806fe
Matthew Durocher <matthewdurocher@Matthews-Mac-Studio.local>
2022-10-16 23:12:46 -0700

    create

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+// Import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statment
+funciton printHello() {
+    console.log("Hello there!");
+}
+
+// Takes two numbers and adds them
+function addition(a,b){
+    return a + b;
+}
+addition = (a,b) => a+b;
+
+// Simple JavaScript log statement
+function printHello() {
+    return "Hello there!";
+}
+printHello = () => "Hello there!";
